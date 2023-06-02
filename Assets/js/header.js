cd9605989213edd4ee689718f568bd81b0cfaa0c
Deluca00 <huynhkanh24@gmail.com>
2023-06-02 19:16:48 +0700

    BWD

---
+++ Assets/js/header.js
@@ -0,0 +1,32 @@
+const search = document.querySelector('.js-search-btn')
+const searchInput = document.querySelector('.search-input')
+const closeInput = document.querySelector('.js-search-close');
+
+
+search.addEventListener('click',
+    function (event) {
+        searchInput.classList.add('open')
+    })
+
+closeInput.addEventListener('click',
+    function (event) {
+        searchInput.classList.remove('open')
+    })
+
+const back_color_menu = document.querySelector('.menu')
+window.addEventListener('wheel', function (event) {
+    var delta = event.deltaY; // Lấy giá trị delta cuộn chuột
+    function down() {
+        back_color_menu.classList.add('menu-scroll-down')
+
+    }
+    function up() {
+        back_color_menu.classList.remove('menu-scroll-down')
+
+    }
+    if (delta > 0) {
+        down()
+    } else {
+        up()
+    }
+});
